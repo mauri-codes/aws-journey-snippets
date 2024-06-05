34eaf120f1140e68a4c7c0701763297bfdae704a
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2024-06-05 01:49:24 -0400

    added group membership

--- new_account_admin/main.tf
+++ new_account_admin/main.tf
@@ -6,6 +6,14 @@ resource "aws_iam_group" "admins" {
   name = "admins"
 }
 
+resource "aws_iam_user_group_membership" "membership" {
+  user = aws_iam_user.admin.name
+
+  groups = [
+    aws_iam_group.admins.name,
+  ]
+}
+
 resource "aws_iam_group_policy_attachment" "administrator_access" {
   group      = "${aws_iam_group.admins.name}"
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
