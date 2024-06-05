288d32f1843337d7eb5394f961c561177c9461b4
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2024-06-05 16:41:53 -0400

    fixed small error

--- base_tf_infra/locals.tf
+++ base_tf_infra/locals.tf
@@ -1,4 +1,4 @@
 locals {
   account_id  = data.aws_caller_identity.current.account_id
-  bucket_name = var.bucket_name == "" ? var.bucket_name : "infrastructure-$random_integer.priority.result"
+  bucket_name = var.bucket_name != "" ? var.bucket_name : "infrastructure-$random_integer.priority.result"
 }
