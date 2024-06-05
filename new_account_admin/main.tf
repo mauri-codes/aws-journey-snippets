a17621ffce483464466e86884ff7699bd268e792
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2024-06-05 02:43:32 -0400

    added README

--- new_account_admin/main.tf
+++ new_account_admin/main.tf
@@ -15,14 +15,15 @@ resource "aws_iam_user_group_membership" "membership" {
 }
 
 resource "aws_iam_group_policy_attachment" "administrator_access" {
-  group      = "${aws_iam_group.admins.name}"
+  group      = aws_iam_group.admins.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
 resource "aws_iam_access_key" "access_keys" {
-  user    = aws_iam_user.admin.name
+  user = aws_iam_user.admin.name
 }
 
 resource "aws_iam_user_login_profile" "console_access" {
-  user    = aws_iam_user.admin.name
+  user                    = aws_iam_user.admin.name
+  password_reset_required = true
 }
--- new_account_admin/output.tf
+++ new_account_admin/output.tf
@@ -1,11 +1,11 @@
 output "access_keys_secret" {
-  value = aws_iam_access_key.access_keys.secret
+  value     = aws_iam_access_key.access_keys.secret
   sensitive = true
 }
 output "access_keys_id" {
   value = aws_iam_access_key.access_keys.id
 }
 output "user_password" {
-  value =  aws_iam_user_login_profile.console_access.password
+  value     = aws_iam_user_login_profile.console_access.password
   sensitive = true
 }
