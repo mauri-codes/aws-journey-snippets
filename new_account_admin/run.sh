f09fd85c6dd2c8306613f0a95930b714f4fbb8c2
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2024-06-05 00:54:48 -0400

    added run.sh file

---
+++ new_account_admin/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# cd ~
+if terraform --version ; then
+    echo "Terraform Already Installed"
+else
+    sudo yum install -y yum-utils shadow-utils
+    sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+    sudo yum -y install terraform
+fi
