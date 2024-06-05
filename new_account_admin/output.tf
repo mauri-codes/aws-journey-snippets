8097239133dc4cbdc0c092498cadaf6d34e15071
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2024-06-05 01:15:06 -0400

    added sensitive value 2

--- new_account_admin/output.tf
+++ new_account_admin/output.tf
@@ -1,7 +1,7 @@
 output "access_keys_secret" {
   value = aws_iam_access_key.access_keys.secret
+  sensitive = true
 }
 output "access_keys_id" {
   value = aws_iam_access_key.access_keys.id
-  sensitive = true
 }
