879833dd7a17773873b26f91ea9ade6d4a0a27f0
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2024-08-29 03:30:20 -0400

    fixed error with access token

--- register_account/register.py
+++ register_account/register.py
@@ -4,10 +4,12 @@ import json
 
 alias = os.environ["ALIAS"]
 token = os.environ["TOKEN"]
+access_token = os.environ["ATOKEN"]
 region = os.environ["REGION"]
 role_arn = os.environ["ROLE_ARN"]
 account_id = os.environ["ACCOUNT_ID"]
 env = os.environ["ENV"]
+
 
 if env == "dev":
    url =  "https://apiv2.aws-journey.click/awsAccount"
@@ -20,7 +22,8 @@ def register_account():
       "account_id": account_id,
       "alias": alias,
       "region": region,
-      "role_arn": role_arn
+      "role_arn": role_arn,
+      "access_token": access_token
    }
 
    data = json.dumps(request_data)
--- register_account/run.sh
+++ register_account/run.sh
@@ -4,6 +4,7 @@ export ROLE_ARN="$(terraform output -raw role_arn)"
 export ACCOUNT_ID=`aws sts get-caller-identity --query Account --output text`
 export ALIAS=$ALIAS
 export TOKEN=$TOKEN
+export ATOKEN=$ATOKEN
 export ROLE_ARN=$ROLE_ARN
 export REGION=$REGION
 
